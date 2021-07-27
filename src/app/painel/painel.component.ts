f3eab29c81d3b19c614aed1aeaeb6c0762f75319
Renan Roseno <rosenorenan@gmail.com>
2021-07-27 11:05:53 -0300

    New dinamic of rounds

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frase-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Array<Frase> = FRASES;
+  public resposta: string = '';
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+  public progresso = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+  }
 
   ngOnInit(): void {
   }
 
+  public atualizaFrase(event: Event): void {
+    this.resposta = (<HTMLInputElement>event.target).value
+  }
+
+  public verificarResposta(): void {
+    if (this.resposta.toUpperCase() === this.rodadaFrase.frasePtBr.toUpperCase()) {
+      console.log('Acertou');
+      this.progresso += (100 / this.frases.length);
+      if (this.rodada + 1 < this.frases.length) {
+        this.rodada++;
+        this.rodadaFrase = this.frases[this.rodada];
+      }
+    }
+    else console.log('Errou xuburro');
+  }
 }
